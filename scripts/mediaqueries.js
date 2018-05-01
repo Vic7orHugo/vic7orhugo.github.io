bc1f53d7ce455a0ee3e6954049677aa5cda92f31
Vic7orHugo <Vic7orHugo@users.noreply.github.com>
2018-05-01 11:39:35 -0300

    Updated page design a bit

--- scripts/mediaqueries.js
+++ scripts/mediaqueries.js
@@ -3,6 +3,9 @@
 	30/04/2018 21:33 GMT -03:00
 */  
 
+// Device screen size
+const MEDIA_QUERY = "(max-width: 991px)";
+
 function bodyFontSize(query) {
     if (query.matches) { // If media query matches
         document.body.style.fontSize = "30px";
@@ -15,17 +18,34 @@ function contentPadding(query) {
     if (query.matches) { // If media query matches
         document.querySelector("#about-me").style.padding = "40px 10px";
         document.querySelector("#portfolio").style.padding = "40px 10px";
-        document.querySelector("#contact").style.padding = "40px 10px";
+        document.querySelector("#contact").style.padding = "50px 10px";
     } else {
         document.querySelector("#about-me").style.padding = "40px 15%";
         document.querySelector("#portfolio").style.padding = "40px 15%";
-        document.querySelector("#contact").style.padding = "40px 15%";
+        document.querySelector("#contact").style.padding = "50px 15%";
     }
 }
 
-let mediaQuery = "(max-width: 991px)";
-let bodyQuery = window.matchMedia(mediaQuery);
-let contentQuery = window.matchMedia(mediaQuery);
+function init() {
+	let newWidth = parseInt(document.documentElement.clientWidth) / 2;
+	document.querySelector(".triangle-down").style.borderLeft = newWidth.toString() + "px solid #2B2B2B";
+	document.querySelector(".triangle-down").style.borderRight = newWidth.toString() + "px solid #2B2B2B";
+	document.querySelector(".triangle").style.borderLeft = newWidth.toString() + "px solid rgba(220, 220, 220, .8)";
+	document.querySelector(".triangle").style.borderRight = newWidth.toString() + "px solid rgba(220, 220, 220, .8)";
+}
+
+window.onload = init;
+
+window.addEventListener('resize', (event) => {
+	let newWidth = parseInt(document.documentElement.clientWidth) / 2;
+	document.querySelector(".triangle-down").style.borderLeft = newWidth.toString() + "px solid #2B2B2B";
+	document.querySelector(".triangle-down").style.borderRight = newWidth.toString() + "px solid #2B2B2B";
+	document.querySelector(".triangle").style.borderLeft = newWidth.toString() + "px solid rgba(220, 220, 220, .8)";
+	document.querySelector(".triangle").style.borderRight = newWidth.toString() + "px solid rgba(220, 220, 220, .8)";
+});
+
+let bodyQuery = window.matchMedia(MEDIA_QUERY);
+let contentQuery = window.matchMedia(MEDIA_QUERY);
 
 bodyFontSize(bodyQuery);   			 // Call listener function at run time
 contentPadding(contentQuery);
